8fcb69dd7457f72b6e877f7388086d47f0bf4355
Claire AVILA <avila.claire@gmail.com>
2024-02-02 14:21:09 +0100

    reprise du pattern singleton

--- src/fr/fms/dao/Singleton.java
+++ src/fr/fms/dao/Singleton.java
@@ -7,45 +7,36 @@ import java.sql.SQLException;
 public class Singleton {
 
 
-	private static Singleton instance;
-	public String value;
-	private Connection connection;
+	private static volatile Singleton instance;
+	public String data;
 
-	private Connection Singleton() {
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/Shop","root","fms2024");
-			return connection;
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
+	private Singleton(String data) {
+		this.data =data;
+		
 	}
-
-//méthode qui sert à vérifier qu'il n'y a qu'une seule instance de la classe Singleton dans l'application
-	//ça empêche les conflits 
-	public static Singleton getInstance() {
-		//vérifie si l'onstance de la classe a déjà été créé
+	
+	public static Singleton getInstance (String data) {
+		
+		Singleton result = instance;
 		if (instance == null) {
-			instance = new Singleton();
-		}
-		//renvoie l'existance de l'instance.
-		return instance;
-
-	}
-	public Connection getConnection() {
-		return connection;
-	}
-//ferme la connection
-	public void closeConnection() {
-		try {
-			//vérifie que la connexion n'est pas nulle et que la connection n'est pas fermée
-			if (connection != null && !connection.isClosed()) {
-				connection.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		synchronized (Singleton.class) {
+			result = instance;
+			if (instance == null) {
+				instance = new Singleton(data);
+		
 		}
 	}
 }
-// maintenant faut connecter les dao avec ce pattern
+		return instance;
+		
+	} 
+}
+	
+//le singleton permet le multi threading 
+// retourne les instances existantes si elles ont déjà été créés
+// doit être utilisé quand une classe peut avoir une seule instance disponible
+// rend indisponible la création d'objetcs de classe sauf pour les méthodes static
+	
+
+
 
